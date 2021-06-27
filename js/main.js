6d6db6ab281c8534fa5a79bd9c057b0f05f8a2db
Cristian <cristianarielbaez30@gmail.com>
2021-06-27 14:55:50 -0300

    App-Anime
    PWA anime

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// Chequeo si el browser puede usar Service Worker
+window.addEventListener("load", () => {
+  // Chequeo si el browser puede usar Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../service-worker.js')
+      .then(reg => {
+        console.log("Service worker esta listo!");
+      });
+}
+else {
+  console.log("Service worker no soportado.");
+}
+
+// Event Listener para Offline/ Online Status
+window.addEventListener('offline', event => {
+  document.querySelector('.section-notifications').classList.add('offline');
+  main.innerHTML = "<p>La aplicacion esta offline!</p>"
+});
+
+window.addEventListener('online', event => {
+  document.querySelector('.section-notifications').classList.remove('offline');
+  buscarAnime(event);
+});
+
+// A veces este evento falla, ojo!
+// Sirve para saber si el navegador esta offline, cuando entramos offline. 
+// Es decir, no se disparo los eventos de arriba aun, y necesito conocer el estado.
+// if (!navigator.onLine) {
+
+// mejor:
+const isOnline = async () => {
+  try {
+    // El "cache no-store" es para que el fetch no guarde en cache el request
+    // Si esto pasara, responderia el cache del browser y no el resultado de tener conexion
+    const response = await fetch('https://code.jquery.com/jquery-3.6.0.slim.min.js', {cache: "no-store"});
+    console.log(response.json());
+    
+    if (response.url == "http://127.0.0.1:5500/offline.html"){
+      return false;
+    }
+    
+    return true;
+  } catch (error) {
+    console.error('offline and rejected', error)
+    return false;
+  }
+}
+
+isOnline().then(
+  // Resolve, estamos online
+  resp => {
+    if (resp){
+      // Creo un evento
+      var evento = new CustomEvent("escucharEvent", {});
+
+      // Lo Disparo!
+      document.dispatchEvent(evento);
+    } else {
+      document.querySelector('.section-notifications').classList.add('offline');
+    }
+  },
+  // Reject, estamos offline
+  ()=> {
+    document.querySelector('.section-notifications').classList.add('offline');
+  }
+);
+});
---
+++ service-worker.js
@@ -0,0 +1,133 @@
+// Nombre del Cache
+const cacheName = 'cache-version-1';
+
+// Archivos/Recursos que vamos a "cachear"
+const precache = [
+  './js/main.js',
+  './css/estilos.css',
+  './css/material-kit.css',
+  './index.html',
+  './offline.html',
+  './pacman-master/css/pacman.css',
+  './pacman-master/js/pacman.js',
+  "./img/anime-multitud.jpg",
+  "./img/dragon-ball-super.jpg",
+  "./img/dragon-ball-z.jpg",
+  "./img/kimi-no-na-wa.jpg",
+  "./img/naruto.jpg",
+  "./img/naruto2.jpg",
+  "./img/naruto3.jpg",
+  "./img/naruto4.jpg",
+  "./img/pokemon.jpg",
+  "./img/pokemon2.jpg",
+  "./img/sword-art.jpg",
+  "./img/icons/logo192x192.png",
+  "./img/icons/logo512x512.png",
+  "./img/icons/inicio.png",
+  "./img/descarga.png",
+  "./img/icons/video.png",
+  "./img/icons/inicio.svg",
+  "./img/descarga.svg",
+  "./img/icons/video.svg",
+  './img/icons/enviar.svg',
+  './img/icons/link.svg',
+  './img/icons/sinopcis.svg',
+  './js/material-kit.js',
+  './js/api.js',
+  './js/core/bootstrap-material-design.min.js',
+  './js/core/jquery.min.js',
+  './js/core/popper.min.js'
+];
+
+// Instalación
+self.addEventListener('install', event => {
+
+  // Hago a este SW el activo, matando otros
+  // Sino quedan caches inactivos 
+  self.skipWaiting();
+
+  event.waitUntil(
+      // Abro el cache, entonces agrego los archivos/recursos
+      caches.open(cacheName).then(cache => {
+        return cache.addAll(precache)
+      })
+  );
+});
+
+
+// Update - Es decir, si cambia una parte del SW (nombre), updatea el cache 
+self.addEventListener('activate', event => {
+
+  const cacheWhitelist = [cacheName];
+
+  // Esto es lo que updatea cada una de las keys en el mapa del caché
+  event.waitUntil(
+      // Tomo las keys y las paso para revisar individualmente
+      caches.keys().then(cacheNames => {
+        // devuelvo Promesa
+        return Promise.all(
+            // Hago un map, para borrar key individualmente.
+            // Recuerden que era el update, asi que precisa un delete.
+            cacheNames.map(cacheName => {
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+              }
+            })
+        )
+      })
+  );
+});
+
+
+// Chequeamos la response
+function shouldAcceptResponse(response) {
+    return response.status !== 0 && !(response.status >= 400 && response.status < 500) || 
+        response.type === 'opaque' || 
+        response.type === 'opaqueredirect';
+}
+
+// Creamos el cache a partir de fetch de recursos
+self.addEventListener('fetch', event => {
+  // Chequeamos si existe en cache para el render de pagina
+  // sino vamos a hacer cache del nuevo request
+  event.respondWith(
+      caches.open(cacheName).then(cache => { // Abrimos el cache actual
+        return cache.match(event.request).then(response => {
+          
+          // Matcheo! - return response, se lo pasamos al promise abajo
+          if (response) {
+            return response;
+          }
+
+        // Tomamos el response cache de arriba
+        return fetch(event.request).then(
+          function(response) {
+
+            // Chequeamos si recibimos una respuesta valida
+            if(shouldAcceptResponse(response)) {
+              return response;
+            }
+
+            // Hay que clonar la respuesta
+            // La respuesta es un stream, y como queremos que el browser
+            // consuma la respuesta como si el cache consumiera la respuesta,
+            // necesitamos clonarla para asi tener dos streams: (https://streams.spec.whatwg.org/)
+            var responseToCache = response.clone();
+
+            // Aca lo que hace es guardar los recursos que vinieron del server
+            caches.open(cacheName)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        )
+        }).catch(error => {
+          console.info('Fallo Servise Worket', error); // importantisimo para saber si tenemos un error en algun lado.
+          // si el cache falla, mostramos offline page
+          return caches.match('offline.html');
+        });
+      })
+  );
+});
